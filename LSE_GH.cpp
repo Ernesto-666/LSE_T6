f54436cda56a74a204417fb7a53217c55ab71b93
Ernesto-666 <62909726+Ernesto-666@users.noreply.github.com>
2020-04-04 00:38:24 -0600

    LSE_GH.cpp

--- LSE_GH.cpp
+++ LSE_GH.cpp
@@ -52,19 +52,162 @@ class LSE
 		Nodo *Buscar(int);
 		void Borrar(int);
 };
-
-
-
-
-
-
-
-int main(int argc, const char * argv[])
+void LSE::InsertarI(int a)
 {
-    
-    
-    
-    LDE A;
+if(!Inicio)
+{
+    Inicio=new Nodo(a);
+}else
+{
+	Nodo *A1=new Nodod(a);
+	A1->Asignasig(Inicio);
+	Inicio=A1;
+}
+}
+void LSE::InsertarF(int a)
+{
+	if(Inicio==NULL)
+	{
+	Inicio=new Nodo(a);	
+	}else
+	{
+	Nodo *A2=Inicio;
+		while(A2->Obtienesig()!=NULL)
+		{
+			A2=A2->Obtienesig();
+			Nodo *A3=new Nodod(a);
+			A2->Asignasig(A3);
+		}
+	}
+}
+void LSE::BorrarI()
+{
+	if(!Inicio)
+	{
+	cout<<"La lista esta vacia"<<endl<<endl;
+	}else
+	{
+	 if(Inicio->Obtienesig()==NULL)
+	 {
+	  delete Inicio;
+	  Inicio=NULL;
+	 }else
+	 {
+	 Nodo *A4=Inicio;
+	 Inicio=Inicio->Obtiensig();
+	 A4->Asignasig(NULL);
+	 delete A4;
+	 }
+	}
+}
+void LSE::BorrarF()
+{
+	if(!Inicio)
+	{
+	 cout<<"La lista esta vacia"<<endl<<endl;	
+	}else
+	{
+	 if(Inicio->Obtienesig()==NULL)
+	 {
+	 delete Inicio;
+	 Inicio=NULL;
+	 }else
+	 {
+	 Nodo *A5, *A6;
+	 A5=A6;
+	 while(A5->Obtienesig()==NULL)
+	 {
+	 A6=A5;
+	 A5=A5->Obtienesig();
+	 Inicio=NULL;
+	 }
+         A6->Asignasig(NULL);
+	delete A6;
+	 }
+	}
+}
+void LSE::Imprimir()
+{
+if(!Inicio)
+{
+cout<<"La lista esta vacia"<<endl<<endl;	
+}else
+{
+Nodo *A7=Incio;
+	while(A7!=NULL)
+	{
+	A7->Imprimir();
+		A7=A7->Obtienesig();
+	}
+}
+}
+int LSE::Contar()
+{
+int contar=0;
+if(!Inicio)
+{
+contar=0;	
+}else
+{
+Nodo *A8=Inicio;
+	while(A8=NULL)
+	{
+	contar++;
+	A8=A8->Obtienesig();
+	}
+}
+	return contar;
+}
+Nodo* LSE::Buscar(int a)
+{
+Nodo *buscando=Inicio;
+	if(Inicio)
+	{
+	while(buscando!=NULL)
+	{
+	if(buscando->Obtienedato()==a)	
+	{
+	return buscando;
+		buscando=buscando->Obtiensig();
+	}
+	}
+	}
+	return buscando;
+}
+void LSE::Borrar(int a)
+{
+	Nodo *A9=Buscar(a);
+	if(A9==NULL)
+	{
+	cout<<"El dato no fue encontrado"<<endl<<endl;	
+	}else
+	{
+	if(A9==Inicio)
+	{
+	BorrarI();	
+	}else
+	{
+	if(A9->Obtiensig()==NULL)
+	{
+	BorrarF();	
+	}else
+	{
+	Nodo *borrando=Inicio;
+		while(borrando->Obtienesig()!=A9)
+		{
+		borrando=borrando->Obtiensig();
+			borrando->Asignasig(A9->Obtienesig());
+			A9->Asignasig(NULL);
+			delete A9;
+		}
+	}
+	}
+	}
+}
+
+int main(int argc, const char * argv[]/*¿Para que son estas variables?*/)
+{
+    LSE A;
     int opc, dato;
     do{
         cout<<"1.- Insertar Inicio"<<endl;
